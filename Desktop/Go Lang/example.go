839ae9b09c6e32cf7c45f25ff3de1ef569e90b85
Prathyu22 <lakshmiprathyushaveturi@gmail.com>
2023-01-25 09:07:15 +0530

    for_loop-infinite_loop_example

--- Desktop/Go Lang/example.go
+++ Desktop/Go Lang/example.go
@@ -2,23 +2,10 @@ package main
 
 import "fmt"
 
-/*
-
-Data types :
-
-1. bool
-2. string
-3. int, int8, int16, int32, int64.
-   uint, uint8, uint16, uint32, uint64, uintptr.
-   --> Here, uint refers to "unsigned integer".
-4. byte // alias for uint8.
-5. rune // alias for int32.
-        // represents a unicode code point.
-6. float32, float64
-
-*/
+// only one looping statement is available in "go language." --> "for"
 func main() {
-	const num int = 9
-	num = 10 // error for reinitializing.
-	fmt.Print(num)
+	//infinite loop
+	for {
+		fmt.Println("Example")
+	}
 }
