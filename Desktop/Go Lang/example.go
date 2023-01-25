9b545021187979b7ecd78b657a508c8c792cc0c9
Prathyu22 <lakshmiprathyushaveturi@gmail.com>
2023-01-25 09:08:40 +0530

    method2 for infinite loop

--- Desktop/Go Lang/example.go
+++ Desktop/Go Lang/example.go
@@ -4,8 +4,9 @@ import "fmt"
 
 // only one looping statement is available in "go language." --> "for"
 func main() {
-	//infinite loop
-	for {
+	// method2 for infinite loop
+	i := 1
+	for i <= 5 {
 		fmt.Println("Example")
 	}
 }
