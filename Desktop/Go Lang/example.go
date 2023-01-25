a79263b3374298c70adb916084c3da0349904338
Prathyu22 <lakshmiprathyushaveturi@gmail.com>
2023-01-25 08:55:47 +0530

    declaration of variables and ways of printing

--- Desktop/Go Lang/example.go
+++ Desktop/Go Lang/example.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+/*
+
+Data types :
+
+1. bool
+2. string
+3. int, int8, int16, int32, int64.
+   uint, uint8, uint16, uint32, uint64, uintptr.
+   --> Here, uint refers to "unsigned integer".
+4. byte // alias for uint8.
+5. rune // alias for int32.
+        // represents a unicode code point.
+6. float32, float64
+
+*/
 func main() {
-	fmt.Print(2 + 3)
+	var num = 2
+	var num1 int = 3
+	var num2 int
+	num2 = 4
+	fmt.Println(num)
+	fmt.Println(num1)
+	fmt.Println(num2)
+	fmt.Print(num, num1, num2)
 }
