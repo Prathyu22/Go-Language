2bc0cd39ce663d38da879f76b30f6b8eb549ea96
Prathyu22 <lakshmiprathyushaveturi@gmail.com>
2023-01-25 08:48:21 +0530

    print_statement

--- Desktop/Go Lang/example.go
+++ Desktop/Go Lang/example.go
@@ -1,12 +1,7 @@
-// Everything we code in "go" should be placed in a package.
 package main
 
-//importing anything should be used otherwise we encounter an error.
-// "fmt" stands for "format".
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World")
+	fmt.Print(2 + 3)
 }
-
-//Execute in the terminal using the command -> "go run example.go" as the file name is example.
