11cf673aee3b4da712c3fb637cf770460e500086
Prathyu22 <lakshmiprathyushaveturi@gmail.com>
2023-01-25 11:17:34 +0530

    precision

--- Desktop/Go Lang/example.go
+++ Desktop/Go Lang/example.go
@@ -6,8 +6,10 @@ import (
 )
 
 func main() {
-	var num float64 = 9
-	fmt.Println("Example")
-	var result = math.Sqrt(num) // math.Sqrt() takes only float type numbers.
-	fmt.Println(result)
+	var num float64 = 12
+	var result = math.Sqrt(num)                      // math.Sqrt() takes only float type numbers.
+	fmt.Printf("The output is %g Thank you", result) //Printf is used to give the precision.
+	//output: The output is 3.4641016151377544 Thank you
+	fmt.Printf("The output is %.2f Thank you", result) // upto 2 decimals.
+	//output: The output is 3.46 Thank you
 }
