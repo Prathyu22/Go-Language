0c2b19ecd7c5e92d0d68b3f5b7118347c2259c20
Prathyu22 <lakshmiprathyushaveturi@gmail.com>
2023-01-25 12:21:21 +0530

    Switchcase

--- Desktop/Go Lang/example.go
+++ Desktop/Go Lang/example.go
@@ -5,13 +5,17 @@ package main
 import "fmt"
 
 func main() {
-	num := 9
-	if num < 5 {
-		fmt.Println("Hi ", num)
-	} else if num == 0 {
-		fmt.Println("Bye ", num)
-	} else {
-		fmt.Println("Good Bye ", num)
+	num := 3
+
+	switch num {
+	case 1:
+		fmt.Println("One")
+	case 2:
+		fmt.Println("Two")
+	case 3:
+		fmt.Println("Three")
+	default:
+		fmt.Println("")
 	}
 
 }
