29b5fc7a035dc067152b624b8965bd1035813d10
Prathyu22 <lakshmiprathyushaveturi@gmail.com>
2023-01-25 10:55:22 +0530

    variable scope_local level(function level)

--- Desktop/Go Lang/example.go
+++ Desktop/Go Lang/example.go
@@ -5,11 +5,13 @@ import "fmt"
 var a = 9 //this is package level variable declaration.
 
 func demo() {
-	fmt.Println(a) //executes.
+	var a = 8
+	// function level variable is called as local variable and it has more priority to execute than package level.
+	fmt.Println("in demo: ", a) //executes.
 }
 
 //functions
 func main() {
 	demo()
-	fmt.Println(a) // now it executes without any error since varibale is declared in package level.
+	fmt.Println("in main(): ", a) // now it executes without any error since varibale is declared in package level.
 }
