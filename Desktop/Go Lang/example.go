fd9063d719c4e09e3e0ee831780e6ba6029401ff
Prathyu22 <lakshmiprathyushaveturi@gmail.com>
2023-01-25 12:11:47 +0530

    if-else

--- Desktop/Go Lang/example.go
+++ Desktop/Go Lang/example.go
@@ -1,15 +1,15 @@
+//https://pkg.go.dev/fmt -- to get the overview about fmt.
+
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 func main() {
-	var num float64 = 12
-	var result = math.Sqrt(num)                      // math.Sqrt() takes only float type numbers.
-	fmt.Printf("The output is %g Thank you", result) //Printf is used to give the precision.
-	//output: The output is 3.4641016151377544 Thank you
-	fmt.Printf("The output is %.2f Thank you", result) // upto 2 decimals.
-	//output: The output is 3.46 Thank you
+	num := 9
+	if num < 5 {
+		fmt.Println("Hi ", num)
+	} else {
+		fmt.Println("Bye ", num)
+	}
+
 }
